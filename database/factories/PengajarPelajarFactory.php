5fb7658213fdb2f70949430491efcf8bc74ccb37
soretaka <yatesthap@gmail.com>
2022-06-02 11:41:14 +0700

    feat: init project

---
+++ database/factories/PengajarPelajarFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\pengajar_pelajar>
+ */
+class PengajarPelajarFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
