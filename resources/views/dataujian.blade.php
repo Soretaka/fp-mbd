b3b4e801c489407887b3b1603585998aac98dad3
RavindraWiguna <77233945+RavindraWiguna@users.noreply.github.com>
2022-06-04 00:56:29 +0700

    rename tittle for dataujian view

--- resources/views/dataujian.blade.php
+++ resources/views/dataujian.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Data Ujian</title>
 </head>
 <body>
     <table style="text-indent: 50">
