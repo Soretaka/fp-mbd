ac238ba5cbba25bd42b561e23d6908bff659becf
AiffahKiysa <kiysa.205025@mhs.its.ac.id>
2022-06-08 18:12:42 +0700

    feat:rank

--- resources/views/rank.blade.php
+++ resources/views/rank.blade.php
@@ -3,39 +3,41 @@
 @section('container')
 <body>
     <div class="text-white">
-    <div class="card-body container">
-        <div>Rangking: {{ $ujian[0]->nama}}</div>
-        <table style="text-indent: 50">
-            <tr>
-            <td>Rank</td>
-            <td>Pelajar_id</td>
-            <td>Nama Pelajar</td>
-            <td>Ujian_id</td>
-            <td>Nilai</td>
-            </tr>
-            @php
-                $i=1;
-            @endphp
-            @foreach ($pelajar as $p)
-            <tr>
-            <td>{{ $i }}</td>
-            <td>{{ $p->pelajar_id }}</td>
-            <td>{{ $p->nama }}</td>
-            <td>{{ $p->ujian_id }}</td>
-            <td>{{ $p->nilai }}</td>
-            </tr>
-            @php
-                $i+=1;
-            @endphp
-            @endforeach
-        </table>
-        <br>
+        <div class="card shadow mb-4">
+            <div class="card-body">
+                <div>Rangking: {{ $ujian[0]->nama}}</div>
+                <br>
+                <div class="table-responsive">
+                    <table class="table table-bordered table-hover" id="dataTable" width="50%" cellspacing="0">
+                        <thead>
+                        <tr>
+                        <td>Rank</td>
+                        <td>Pelajar_id</td>
+                        <td>Nama Pelajar</td>
+                        <td>Ujian_id</td>
+                        <td>Nilai</td>
+                        </tr>
+                        @php
+                            $i=1;
+                        @endphp
+                        @foreach ($pelajar as $p)
+                        <tr>
+                        <td>{{ $i }}</td>
+                        <td>{{ $p->pelajar_id }}</td>
+                        <td>{{ $p->nama }}</td>
+                        <td>{{ $p->ujian_id }}</td>
+                        <td>{{ $p->nilai }}</td>
+                        </tr>
+                        @php
+                            $i+=1;
+                        @endphp
+                        @endforeach
+                    </table>
+                </div>
+            </div>
+            </div>
         {{-- <td>Nilai total ujian = {{ $total_ujian }}</td> --}}
     </div>
 
 </body>
-<<<<<<< HEAD
 @endsection
-=======
-</html>
->>>>>>> 0dec4677d7eec19c73b2b81202f7c25f62c35456
