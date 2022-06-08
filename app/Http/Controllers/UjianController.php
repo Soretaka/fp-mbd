db1ba524bb34abfc950648d40a0558d8c0110159
soretaka <yatesthap@gmail.com>
2022-06-08 10:33:47 +0700

    delete dd

--- app/Http/Controllers/UjianController.php
+++ app/Http/Controllers/UjianController.php
@@ -23,8 +23,6 @@ class UjianController extends Controller
         $soals=soal::where('ujian_id',$pelajar_ujian->ujian_id)->get();
         return view('ujian',[
             'soals' => $soals,
-            // 'ujian' => $pelajar_ujian->ujian_id,
-            // 'pelajar' => $pelajar_ujian->pelajar_id,
             'pelajar_ujian' => $pelajar_ujian,
         ]);
     }
@@ -34,11 +32,9 @@ class UjianController extends Controller
         $ujian_id=$request->ujian_id;
         $pelajar_id=$request->pelajar_id;
         $pelajar_ujian_id=$request->pelajar_ujian;
-        // dd($userAnswers[1]);
         $realAnswers= soal::where('ujian_id',$ujian_id)
                     ->orderBy('id')
                     ->pluck('jawaban');
-        // dd($realAnswers);    
         $counts = soal::where('ujian_id',$ujian_id)->count();
         $ujian= ujian::where('id',$ujian_id)->get()->first();
         $flag=0;
@@ -50,9 +46,7 @@ class UjianController extends Controller
         $flag+=1;
         }
         $nilai=$ansright/$counts * 100;
-        // dd($pelajar_ujian_id);
         $ujian_pelajaran=pelajar_ujian::findOrFail($pelajar_ujian_id);
-        // dd($ujian_pelajaran);
         if($nilai > $ujian->kkm){
             $status = 1;
         }else{
@@ -66,7 +60,6 @@ class UjianController extends Controller
             "status" => $status
         ];
         DB::table('pelajar_ujians')->where('id',$ujian_pelajaran->id)->update($new_data);
-        // $ujian_pelajaran=update($new_data);
         return view('tampilkan_nilai',[
             'nilai' => $nilai,
         ]);
